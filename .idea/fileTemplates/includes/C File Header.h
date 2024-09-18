bec3b3708ca2debd2c7b8433021f979564029826
Computery <77170025+C0mputery@users.noreply.github.com>
2024-09-17 23:14:21 -0400

    Updated git ignore

---
+++ .idea/fileTemplates/includes/C File Header.h
@@ -0,0 +1,7 @@
+﻿#if ($HEADER_COMMENTS)
+/// Created by $USER_NAME on ${DATE}.
+#if ($ORGANIZATION_NAME && $ORGANIZATION_NAME != "")
+/// Copyright (c) $YEAR ${ORGANIZATION_NAME}#if (!$ORGANIZATION_NAME.endsWith(".")).#end All rights reserved.
+#end
+#end
+
