5640f633c4d967268e4f0c8e66fac361e764c810
Computery <77170025+C0mputery@users.noreply.github.com>
2024-10-21 16:28:42 -0400

    AAAAAAAAAAAAAA

--- src/Electronics/Types/GpsBase.h
+++ src/Electronics/Types/GpsBase.h
@@ -1,5 +1,6 @@
 ﻿#ifndef GPSBASE_H
 #define GPSBASE_H
+#include "Electronic.h"
 
 namespace Electronics::Types {
     enum class GpsFix {
@@ -7,10 +8,12 @@ namespace Electronics::Types {
         Fix,
     };
 
-    class GpsBase {
+    class GpsBase : public Electronics {
     public:
         virtual ~GpsBase() = default;
+        virtual void Init() = 0;
         virtual GpsFix Update() = 0;
+        virtual GpsFix GetFix() = 0;
         virtual double GetLatitude() = 0;
         virtual double GetLongitude() = 0;
         virtual double GetSpeed() = 0;
