3a09118cac95f7e86ec31679d7f8aafbe121b850
Computery <77170025+C0mputery@users.noreply.github.com>
2024-11-08 14:15:14 -0500

    Cleaned Up electoric code

---
+++ src/Electronics/Implementations/GPS/UBlox/UbloxGpsBase.cpp
@@ -0,0 +1,61 @@
+﻿/// Created by Computery on 11/5/2024.
+
+#include "UbloxGpsBase.h"
+#include <Logging/Logger.h>
+
+using namespace Logging;
+using namespace Electronics::Types;
+
+void UbloxGpsBase::Update() {
+    Logger::Log(F("Updating GPS..."));
+
+    // 0=no fix, 1=dead reckoning, 2=2D, 3=3D, 4=GNSS, 5=Time fix
+    const uint8_t fixType = m_UbloxGnss.getFixType();
+    if (fixType == 0 || fixType == 5) {
+        m_Fix = NoFix;
+    } else if (fixType == 1) {
+        m_Fix = DeadReckoning;
+    } else {
+        m_Fix = Fix;
+    }
+
+    if (m_Fix == NoFix) {
+        Logger::Log(F("GPS data invalid or unavailable... keeping old data."));
+        return;
+    }
+
+    const double latitude = static_cast<double>(m_UbloxGnss.getLatitude()) / 10000000.0;
+    const double longitude = static_cast<double>(m_UbloxGnss.getLongitude()) / 10000000.0;
+
+    if (latitude == 0 || longitude == 0) {
+        Logger::Log(F("GPS data invalid or unavailable... keeping old data."));
+        return;
+    }
+
+    m_Latitude = latitude;
+    m_Longitude = longitude;
+    m_Speed = static_cast<double>(m_UbloxGnss.getGroundSpeed()) / 1000.0;
+    m_Heading = static_cast<double>(m_UbloxGnss.getHeading()) / 100000.0;
+}
+
+GpsFix UbloxGpsBase::GetFix() {
+    return m_Fix;
+}
+
+double UbloxGpsBase::GetLatitude()  {
+    return m_Latitude;
+}
+
+double UbloxGpsBase::GetLongitude()  {
+    return m_Longitude;
+}
+
+double UbloxGpsBase::GetSpeed() {
+    return m_Speed;
+}
+
+double UbloxGpsBase::GetHeading() {
+    return m_Heading;
+}
+
+
---
+++ src/Electronics/Implementations/GPS/UBlox/UbloxGpsBase.h
@@ -0,0 +1,26 @@
+﻿/// Created by Computery on 10/28/2024.
+
+#pragma once
+
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h>
+#include "Electronics/Types/Gps/GpsBase.h"
+
+namespace Electronics::Types {
+    class UbloxGpsBase : public GpsBase {
+    public:
+        void Update() override;
+        GpsFix GetFix() override;
+        double GetLatitude() override;
+        double GetLongitude() override;
+        double GetSpeed() override;
+        double GetHeading() override;
+
+    protected:
+        double m_Speed = 0;
+        GpsFix m_Fix = NoFix;
+        double m_Latitude = 0;
+        double m_Longitude = 0;
+        double m_Heading = 0;
+        SFE_UBLOX_GNSS m_UbloxGnss;
+    };
+}
