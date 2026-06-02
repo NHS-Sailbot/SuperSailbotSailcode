2bfb40e6bcc36811fd8c90fdc60009fea971ce49
lucas <litrato28@newburyportschools.com>
2026-06-02 09:58:08 -0400

    why was this baud different what

--- src/Electronics/ElectronicsManager.cpp
+++ src/Electronics/ElectronicsManager.cpp
@@ -55,7 +55,7 @@ namespace Electronics {
                 .facingVector = {1.0f, 0.0f, 0.0f}
             }, Wire1, 0x68);
 
-        WindSensor = new WindSensors::FancyWindSensor(0.0, Serial3, 115200);
+        WindSensor = new WindSensors::FancyWindSensor(0.0, Serial3, 38400);
 
 
         Logger::Log(F("Electronics started!"));
